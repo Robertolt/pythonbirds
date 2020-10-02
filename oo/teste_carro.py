a4079fec232023cbda67ab6042bf77146c56b620
Robertolt <rl.beto.lorenzoni@gmail.com>
2020-10-02 19:08:11 -0300

    feita umas alteracoes

--- oo/teste_carro.py
+++ oo/teste_carro.py
@@ -4,9 +4,9 @@ from oo.carro import Motor
 
 class CarroTestCase(TestCase):
     def teste_velocidade_inicial(self):
-       motor = Motor
+       motor = Motor()
        self.assertEqual(0,motor.velocidade)
 
     def teste_acelerar(self):
-       motor = Motor
+       motor = Motor()
        self.assertEqual(1,motor.velocidade)
